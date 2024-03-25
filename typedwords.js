978ba6f43b7629722aa2d2f000e9c28201d4c09b
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-03-25 01:48:42 +0100

    script file linked with typedwords html

--- typedwords.js
+++ typedwords.js
@@ -2,6 +2,7 @@ const Input = document.querySelector("input");
 const typedLetter = document.querySelector("p");
 
 Input.addEventListener("keydown", e =>{
-    typedLetter += `You typed ${e.key}`
+    typedLetter.textContent = `You typed "${e.key}"`
 
-})
+});
+
